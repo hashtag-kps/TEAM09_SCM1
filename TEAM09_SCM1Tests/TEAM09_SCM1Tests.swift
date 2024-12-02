f60f4b85209ec0a5db4364a29df71a240c812cae
Kavyansh Pratap Singh <kavyansh.22scse1300013@galgotiasuniversity.edu.in>
2024-12-02 12:16:02 +0530

    Initial Commit

---
+++ TEAM09_SCM1Tests/TEAM09_SCM1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  TEAM09_SCM1Tests.swift
+//  TEAM09_SCM1Tests
+//
+//  Created by Batch - 2 on 02/12/24.
+//
+
+import Testing
+@testable import TEAM09_SCM1
+
+struct TEAM09_SCM1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
